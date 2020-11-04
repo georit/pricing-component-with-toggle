2899f2ac3f257ca7ae83550af4fb19def3c7ef66
George Mapaya <mambostudioz@gmail,com>
2020-11-03 18:14:26 -0600

    Add functionality to toggle switch

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 /* *****Variables***** */
+// const annualPrices = ["$199.99", "$249.99", "$399.99"];
+// const monthlyPrices = ["$19.99", "$24.99", "$39.99"];
+
+const basicPrice = document.getElementById("price-b");
+const proPrice = document.getElementById("price-pro");
+const masterPrice = document.getElementById("price-master");
+const toggleSwitch = document.getElementById("toggle-switch");
 /* *****Functions***** */
 /* *****Event listeners***** */
+
+toggleSwitch.addEventListener("change", () => {
+  if (!toggleSwitch.checked) {
+    basicPrice.innerText = "$199.99";
+    proPrice.innerText = "$249.99";
+    masterPrice.innerText = "$399.99";
+  } else {
+    basicPrice.innerText = "$19.99";
+    proPrice.innerText = "$24.99";
+    masterPrice.innerText = "$39.99";
+  }
+});
